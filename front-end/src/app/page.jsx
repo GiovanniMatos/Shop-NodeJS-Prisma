31b36868b8735d042ef2a7c8d02e58bae5439b44
GiovanniMatos <giovannyafonso23@gmail.com>
2025-04-01 17:09:24 -0300

    front-end NextJS

---
+++ front-end/src/app/page.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+      <div>
+        <h1>Bem-vindo à Loja</h1>
+        <p>Esta é a página inicial usando o App Router.</p>
+        <a href="/login">Ir para Login</a>
+      </div>
+    );
+  }
